ef6793c6aa1a2e5613d8b54a99fd8000c894b2d7
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-09-28 13:15:19 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 let current_temperature = 0
+// name:Magnus 
+// date: 9/28/2020
+// code: shows the current temperature after a 1s delay
+//  
 basic.forever(function () {
     current_temperature = input.temperature()
     basic.showNumber(current_temperature)
